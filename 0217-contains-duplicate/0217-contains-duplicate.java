ed0794aaa851dccf9c5576698cfe92ab7b1f7c65
gurpreetkaur2309 <128304835+gurpreetkaur2309@users.noreply.github.com>
2024-11-04 17:34:58 +0530

    Time: 21 ms (9.95%), Space: 55.5 MB (78.11%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,30 +1,48 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       HashMap<Integer, Integer> hm = new HashMap<>(); 
+    //    HashMap<Integer, Integer> hm = new HashMap<>(); 
        
-       int count=0;
+    //    int count=0;
       
-        for(int i=0;i<nums.length;i++){
+    //     for(int i=0;i<nums.length;i++){
             
-            count=1;
+    //         count=1;
             
-            if(hm.containsKey(nums[i])){
-                 count++;
-                hm.put(nums[i],count);
+    //         if(hm.containsKey(nums[i])){
+    //              count++;
+    //             hm.put(nums[i],count);
+    //         }
+    //         else{
+    //             hm.put(nums[i],count);
+    //         }
+    //         // if(nums.length%2!=0){
+    //         //     return nums[]
+    //         // }
+    //     }
+    //     if(hm.containsValue(2)){
+    //         return true;
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    boolean flag=false;
+    Arrays.sort(nums);
+           for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+            flag=true;
+            return true;
             }
             else{
-                hm.put(nums[i],count);
-            }
-            // if(nums.length%2!=0){
-            //     return nums[]
-            // }
-        }
-        if(hm.containsValue(2)){
-            return true;
-        }
-        else{
-            return false;
-        }
+                flag=false;
+            } 
+                    
+             }
+             if(flag==true){
+                return true;
+             }
+             else{
+                return false;
+             }
            
 }
        
