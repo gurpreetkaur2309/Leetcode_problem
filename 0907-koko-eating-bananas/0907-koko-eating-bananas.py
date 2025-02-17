9f9027fb2d84189876b5e8a63c00abb9be99fbd4
gurpreetkaur2309 <128304835+gurpreetkaur2309@users.noreply.github.com>
2025-02-17 07:48:19 +0530

    Time: 197 ms (18.71%), Space: 18.9 MB (76.19%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        s=0
+        h1=0
+        f1=0.0
+        n=len(piles)
+        low=1
+        
+        piles.sort()
+        high=piles[n-1]
+        
+        
+        def gogo(piles,mid):
+            h1=0
+            f1=0.0
+            for i in range(0,n):
+                h1=h1 + math.ceil(piles[i] / mid )
+                
+            return h1
+        while(low<=high):
+            mid=(low+high)//2
+            ans=gogo(piles,mid)
+            
+            if(ans<=h):
+                s=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return s
+        
